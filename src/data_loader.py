f1a940d88c5f93bd487db22505c8ea8a026ce7e8
Dennis Fashimpaur <dfashimpaur@gmail.com>
2025-12-02 15:10:01 -0600

    updated to accept str, byte, PathLike, and DataFrame types

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,12 +1,21 @@
-import pandas as pd
+from os import PathLike
 from pathlib import Path
 
+from pandas import DataFrame, read_csv
 
-def load_csv(file_path: str) -> pd.DataFrame:
+
+def load_csv(source: str | PathLike | DataFrame) -> DataFrame:
+    # if it already is a Dataframe, return it
+    if isinstance(source, DataFrame):
+        return source.copy()
+
+    # Otherwise, assume it is a file path
+    if not isinstance(source, (str, bytes, PathLike)):
+        raise TypeError(f"source must be a file path or a pandas dataframe: {type(source)}")
+
     # Check if file exists
-    if not Path(file_path).exists():
-        raise FileNotFoundError(f"File {file_path} does not exist.")
-    # Load CSV into DataFrame
-    dataframe = pd.read_csv(file_path)
-    # Return DataFrame
-    return dataframe
+    if not Path(source).exists():
+        raise FileNotFoundError(f"File {source} not found.")
+
+    # Load CSV into DataFrame and return items-endpoint DataFrame
+    return read_csv(source)
